c7ff0a835d1b50d5d99664713761abe7002118df
Andy <4ndygeiss@gmail.com>
2024-08-09 16:48:18 +0200

    test: add middleware test

---
+++ middleware/default_test.go
@@ -0,0 +1,47 @@
+package middleware_test
+
+import (
+	"andygeiss/htmx-go/integration"
+	"andygeiss/htmx-go/middleware"
+	"andygeiss/htmx-go/usecases/authentication"
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func mockupDefaultHandler(am authentication.Manager) http.HandlerFunc {
+	cfg := &integration.Config{AuthenticationManager: am, ExcludedResources: []string{"/"}}
+	return middleware.Default(cfg, func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("secure"))
+	})
+}
+
+func TestDefault(t *testing.T) {
+	// Arrange
+	am := authentication.NewDefaultManager()
+	r := httptest.NewRequest(http.MethodGet, "/", nil)
+	w := httptest.NewRecorder()
+	token := am.GenerateToken("test")
+	r.Header.Set("Accept-Encoding", "gzip")
+	r.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
+	// Act
+	mockupDefaultHandler(am).ServeHTTP(w, r)
+	// Assert
+	res := w.Result()
+	defer res.Body.Close()
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		t.Errorf("Error should be nil, but got [%s]", err.Error())
+	}
+	if res.StatusCode != http.StatusOK {
+		t.Errorf("Status should be ok, but got [%s]", res.Status)
+	}
+	if res.Header.Get("Content-Encoding") != "gzip" {
+		t.Error("Content-Encoding should be gzip")
+	}
+	if len(data) != 30 {
+		t.Errorf("Data length should be correct, but got [%d]", len(data))
+	}
+}
